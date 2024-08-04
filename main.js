a3a411d7ad3cedc52cd5f04e8e61fa6a274fae65
nathancartlidge <11426516+nathancartlidge@users.noreply.github.com>
2024-08-04 12:40:26 +0100

    style: better display of partial matches (show the part afterwards!)
    also: fixed sticky bug

--- main.js
+++ main.js
@@ -10,15 +10,18 @@ fetch("domains.txt")
         domains = text.split("\n").slice(1, -1).map((domain) => domain.toLowerCase());
         domains.forEach((domain) => {
             let el = document.createElement("p");
-            let url = document.createElement("span");
+            let pre = document.createElement("span");
+            let post = document.createElement("span");
             let link = document.createElement("a");
             el.innerText = "." + domain;
             el.id = "domain_" + domain;
-            url.id = "domain_url_" + domain;
+            pre.id = "domain_pre_" + domain;
+            post.id = "domain_post_" + domain;
             link.innerText = "(wiki)";
-            link.href = "https://en.wikipedia.org/wiki/." + domain;
-            el.appendChild(url);
-            el.appendChild(link)
+            link.href = "https://icannwiki.org/." + domain;
+            el.appendChild(pre);
+            el.appendChild(post);
+            el.appendChild(link);
             domains_div.appendChild(el);
         });
         filterDomains();
@@ -48,18 +51,21 @@ function ideate() {
     }
 }
 
-function makeElement(prefix, domain, is_partial) {
+function makeElement(prefix, domain, postfix) {
     let el = document.createElement("p");
-    let url = document.createElement("span");
+    let pre = document.createElement("span");
+    let post = document.createElement("span");
     let link = document.createElement("a");
     el.innerText = "." + domain;
-    url.innerText = prefix;
-    if (is_partial) {
+    pre.innerText = prefix;
+    post.innerText = postfix;
+    if (postfix !== "") {
         el.classList.add("partial")
     }
     link.innerText = "(wiki)";
-    link.href = "https://en.wikipedia.org/wiki/." + domain;
-    el.appendChild(url);
+    link.href = "https://icannwiki.org/." + domain;
+    el.appendChild(pre);
+    el.appendChild(post);
     el.appendChild(link);
     return el;
 }
@@ -68,11 +74,14 @@ function getMatch(word) {
     let partial = null;
     for (let domain of domains) {
         if (word.length > domain.length && word.endsWith(domain)) {
-            return [word.slice(0, -domain.length), domain, false];
+            return [word.slice(0, -domain.length), domain, ""];
         } else if (partial === null && word.length > domain.length + 1 && word.slice(0, -1).endsWith(domain)) {
-            // note: we do not treat as a partial word if this shortened version exists in the dictionary
-            partial = [word.slice(0, -domain.length - 1), domain,
-                !words.includes(word.slice(0, -1) + domain)];
+            if (words.includes(word.slice(0, -1))) {
+                // we do not treat as a partial word if the shortened version is also a word
+                return [word.slice(0, -domain.length - 1), domain, ""];
+            } else {
+                partial = [word.slice(0, -domain.length - 1), domain, "/" + word.slice(-1)];
+            }
         }
     }
     return partial;
@@ -91,19 +100,23 @@ function filterDomains() {
 
         domains.forEach((domain) => {
             let domain_el = document.getElementById("domain_" + domain);
-            let domain_url_el = document.getElementById("domain_url_" + domain);
+            let domain_pre_el = document.getElementById("domain_pre_" + domain);
+            let domain_post_el = document.getElementById("domain_post_" + domain);
 
             if (search_value.length > domain.length && search_value.endsWith(domain)) {
-                domain_url_el.innerText = search_value.slice(0, -domain.length);
+                domain_pre_el.innerText = search_value.slice(0, -domain.length);
+                domain_post_el.innerText = "";
                 domain_el.classList.remove("hide", "partial");
                 count += 1;
             } else if (search_value.length > domain.length + 1 && search_value.slice(0, -1).endsWith(domain)) {
-                domain_url_el.innerText = search_value.slice(0, -domain.length-1);
+                domain_pre_el.innerText = search_value.slice(0, -domain.length-1);
                 domain_el.classList.remove("hide");
-                domain_el.classList.add("partial")
+                domain_el.classList.add("partial");
+                domain_post_el.innerText = "/" + search_value.slice(-1);
                 count += 1;
             } else if (search_value.length <= domain.length && domain.includes(search_value)) {
-                domain_url_el.innerText = "";
+                domain_pre_el.innerText = "";
+                domain_post_el.innerText = "";
                 domain_el.classList.remove("hide", "partial");
                 count += 1;
             } else {
